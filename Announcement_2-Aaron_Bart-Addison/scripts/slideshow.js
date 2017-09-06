7905bd694770b6bc232f9b0146b13a685fae7b35
Ron Addy <abartaddison12@gmail.com>
2017-09-06 11:55:57 -0400

    Add files via upload

---
+++ Announcement_2-Aaron_Bart-Addison/scripts/slideshow.js
@@ -0,0 +1,27 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+
+function showDivs(n) {
+    
+    var x = document.getElementsByClassName("slideShow");
+        
+        if (n > x.length) {
+            slideIndex = 1;
+        }
+    
+        if (n < 1){
+            slideIndex = x.length;
+        }
+        
+        for (var i = 0; i < x.length; i++){
+            x[i].style.display = "none";
+        }
+    x[slideIndex -1].style.display = "block";
+}
+
+
